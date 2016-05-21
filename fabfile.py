6e38d996782f848b70a626f7cc2d75b58d325710
akshay bhat <akshayubhat@gmail.com>
2016-05-21 02:50:36 -0400

    Removed all python code.

--- fabfile.py
+++ fabfile.py
@@ -7,29 +7,10 @@ logging.basicConfig(level=logging.INFO,
                     datefmt='%m-%d %H:%M',
                     filename='logs/fab.log',
                     filemode='a')
-sys.path.insert(1, os.path.join(os.path.abspath('.'), 'appcode/vendor'))
-vendor_dir = os.path.join(os.path.dirname(__file__), 'appcode/vendor')
 
+"""
+Code no longer uses python.
+"""
 
-@task
-def test():
-    """
-    Test GAE version
-    """
-    local('dev_appserver.py .')
-
-@task
-def deploy():
-    """
-    Deploy GAE version
-    """
-    local('appcfg.py --oauth2 update . ')
-
-@task
-def clear():
-    """
-    remove logs
-    """
-    local('rm logs/*.log &')
 
 
